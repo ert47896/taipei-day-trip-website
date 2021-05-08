ddb86bafda2f5b1de83ff3a28a6c7638d3a71aeb
Tim Lee <krunch522@gmail.com>
2021-05-09 01:34:01 +0800

    Adjust attraction.js and index.js into Model-View-Controller.

--- public/js/attraction.js
+++ public/js/attraction.js
@@ -1,100 +1,136 @@
-let imageNumberNow=0;
-let imageNumberTotal=0;
-window.addEventListener("load", init(window.location.pathname));
-function init(id){
-    let src=window.location.origin+"/api"+id;
-    fetch(src).then((response)=>{
-        return response.json();
-    }).then((result)=>{
-        showData(result.data);
-    })
-}
-function showData(data){
-    let siblingForm=document.querySelector("#orderForm");
-    let title=document.createElement("div");
-    title.classList.add("title");
-    title.textContent=data.name;
-    siblingForm.insertAdjacentElement("beforebegin", title);
-    let subtitle=document.createElement("div");
-    subtitle.classList.add("subTitle");
-    subtitle.textContent=data.category+" at "+data.mrt;
-    siblingForm.insertAdjacentElement("beforebegin", subtitle);
-    let parentMain=document.querySelector("#details");
-    let introduce=document.createElement("div");
-    introduce.classList.add("info");
-    introduce.textContent=data.description;
-    parentMain.insertBefore(introduce, parentMain.firstChild);
-    let addressTitle=document.getElementById("address");
-    let address=document.createElement("div");
-    address.classList.add("info");
-    address.textContent=data.address;
-    addressTitle.insertAdjacentElement("afterend", address);
-    let transportTitle=document.getElementById("transport");
-    let transport=document.createElement("div");
-    transport.classList.add("info");
-    transport.textContent=data.transport;
-    transportTitle.insertAdjacentElement("afterend", transport);
-    let buttonLeftsibling=document.getElementById("buttonSwipeLeft");
-    let photoNumberCircle=document.querySelector(".photoNumberCircle");
-    imageNumberTotal=data.images.length;
-    data.images.forEach((image, index)=>{
-        let imageNode=document.createElement("img");
-        imageNode.src=image;
+// Models
+let models={
+    data:null,
+    // 向api索取該id景點資料
+    getData:function(urlId){
+        let src=window.location.origin+"/api"+urlId;   //window.location.origin 伺服器主機網址
+        return fetch(src).then((response)=>{
+            return response.json();
+        }).then((result)=>{
+            this.data=result.data;
+        });
+    }
+};
+// Views
+let views={
+    // 所呈現圖片序號
+    imageNumberNow:0,
+    // 景點總圖片數
+    imageNumberTotal:0,
+    // 將該id景點資料填入頁面
+    renderData:function(data){
+        let siblingForm=document.querySelector("#orderForm");
+        let title=document.createElement("div");
+        title.classList.add("title");
+        title.textContent=data.name;
+        siblingForm.insertAdjacentElement("beforebegin", title);
+        let subtitle=document.createElement("div");
+        subtitle.classList.add("subTitle");
+        subtitle.textContent=data.category+" at "+data.mrt;
+        siblingForm.insertAdjacentElement("beforebegin", subtitle);
+        let parentMain=document.querySelector("#details");
+        let introduce=document.createElement("div");
+        introduce.classList.add("info");
+        introduce.textContent=data.description;
+        parentMain.insertBefore(introduce, parentMain.firstChild);
+        let addressTitle=document.getElementById("address");
+        let address=document.createElement("div");
+        address.classList.add("info");
+        address.textContent=data.address;
+        addressTitle.insertAdjacentElement("afterend", address);
+        let transportTitle=document.getElementById("transport");
+        let transport=document.createElement("div");
+        transport.classList.add("info");
+        transport.textContent=data.transport;
+        transportTitle.insertAdjacentElement("afterend", transport);
+        let buttonLeftsibling=document.getElementById("buttonSwipeLeft");
+        let photoNumberCircle=document.querySelector(".photoNumberCircle");
+        this.imageNumberTotal=data.images.length;
+        data.images.forEach((image)=>{
+            let imageNode=document.createElement("img");
+            imageNode.src=image;
+            imageNode.classList.add("transparent");
+            buttonLeftsibling.insertAdjacentElement("beforebegin", imageNode);
+            let circleNode=document.createElement("button");
+            circleNode.classList.add("circlePhotoFalse");
+            photoNumberCircle.appendChild(circleNode);
+        });
+        document.getElementById("date").valueAsDate=new Date();     //預設顯示日期為當日， new Date()為當前時間
+    },
+    // 更新與景點圖片相關物件顯示
+    imageRenew:function(indexNow, total){
+        const index=((indexNow % total)+total) % total;             //To obtain a modulo in JavaScript, in place of a % n, use ((a % n ) + n ) % n
+        const imageNode=document.querySelectorAll(".transparent")[index];
+        const circleNode=document.querySelectorAll(".circlePhotoFalse")[index];
+        imageNode.classList.remove("transparent");
+        circleNode.classList.remove("circlePhotoFalse");
+        imageNode.classList.add("opaque");
+        circleNode.classList.add("circlePhotoTrue");
+    },
+    // 移除上一張圖片及小圓圈CSS設定
+    imagePassed:function(){
+        const imageNode=document.querySelector(".opaque");
+        const circleNode=document.querySelector(".circlePhotoTrue");
+        imageNode.classList.remove("opaque");
+        circleNode.classList.remove("circlePhotoTrue");
         imageNode.classList.add("transparent");
-        buttonLeftsibling.insertAdjacentElement("beforebegin", imageNode);
-        let circleNode=document.createElement("button");
         circleNode.classList.add("circlePhotoFalse");
-        photoNumberCircle.appendChild(circleNode);
-    })
-    imageRenew(imageNumberNow, imageNumberTotal);
-}
-let timeIntervalRadio=document.getElementsByName("timeInterval");
-timeIntervalRadio.forEach((radio)=>{
-    radio.addEventListener("click", function(){
+    },
+    // 依據使用者選擇顯示價格
+    changePrice:function(timeSection){
         let oldCostText=document.getElementById("costAmount");
         oldCostText.remove();
         let costLabel=document.getElementById("costTitle");
         let costText=document.createElement("div");
         costText.classList.add("labelText");
-        costText.id = "costAmount";
-        if (this.value==="morning"){                //this就是radio物件
+        costText.id="costAmount";
+        if (timeSection==="morning"){
             costText.textContent="新台幣 2000 元";
         }else{
             costText.textContent="新台幣 2500 元";
-        }
+        };
         costLabel.insertAdjacentElement("afterend", costText);
-    })
-})
-//將日期預設為當日
-document.getElementById("date").valueAsDate=new Date();
-//左滑按鈕
-let leftbtn=document.getElementById("buttonSwipeLeft");
-leftbtn.addEventListener("click", function(){
-    imageNumberNow -= 1;
-    imagePassed();
-    imageRenew(imageNumberNow, imageNumberTotal);
-})
-//右滑按鈕
-let rightbtn=document.getElementById("buttonSwipeRight");
-rightbtn.addEventListener("click", function(){
-    imageNumberNow += 1;
-    imagePassed();
-    imageRenew(imageNumberNow, imageNumberTotal);
-})
-function imageRenew(indexNow, total){
-    const index=((indexNow % total)+total) % total;
-    const imageNode=document.querySelectorAll(".transparent")[index];
-    const circleNode=document.querySelectorAll(".circlePhotoFalse")[index];
-    imageNode.classList.remove("transparent");
-    circleNode.classList.remove("circlePhotoFalse");
-    imageNode.classList.add("opaque");
-    circleNode.classList.add("circlePhotoTrue");
-}
-function imagePassed(){
-    const imageNode=document.querySelector(".opaque");
-    const circleNode=document.querySelector(".circlePhotoTrue");
-    imageNode.classList.remove("opaque");
-    circleNode.classList.remove("circlePhotoTrue");
-    imageNode.classList.add("transparent");
-    circleNode.classList.add("circlePhotoFalse");
-}
+    }    
+};
+// Controllers
+let controllers={
+    // 初始化函式 呈現景點資料與圖片及圓點位置後 啟動物件監聽
+    init:function(){
+        models.getData(window.location.pathname).then(()=>{
+            views.renderData(models.data);
+            views.imageRenew(views.imageNumberNow, views.imageNumberTotal);
+        }).then(()=>{
+            this.timeIntervalListener();
+            this.leftButtonListener();
+            this.rightButtonListener();
+        });
+    },
+    // 監聽使用者選擇上午或下午顯示價格
+    timeIntervalListener:function(){
+        let timeIntervalRadio=document.getElementsByName("timeInterval");
+        timeIntervalRadio.forEach((radio)=>{
+            radio.addEventListener("click", function(){
+                views.changePrice(radio.value);
+            });
+        });
+    },
+    // 監聽使用者點擊左滑按鈕
+    leftButtonListener:function(){
+        let leftbtn=document.getElementById("buttonSwipeLeft");
+        leftbtn.addEventListener("click", function(){
+            views.imageNumberNow -= 1;
+            views.imagePassed();
+            views.imageRenew(views.imageNumberNow, views.imageNumberTotal);
+        });
+    },
+    // 監聽使用者點擊右滑按鈕
+    rightButtonListener:function(){
+        let rightbtn=document.getElementById("buttonSwipeRight");
+        rightbtn.addEventListener("click", function(){
+            views.imageNumberNow += 1;
+            views.imagePassed();
+            views.imageRenew(views.imageNumberNow, views.imageNumberTotal);
+        });
+    }
+};
+controllers.init();     //載入頁面初始化並啟動物件監聽
--- public/js/index.js
+++ public/js/index.js
@@ -1,96 +1,112 @@
-let pageNum=0;
-let loadStatusNone=false;
-let keywordInput=null;
-
-// 進入畫面讀取圖片，執行完結束進入畫面監聽
-window.onload=()=>{
-    loadAttractions(pageNum);
-    window.addEventListener("scroll", scrollLoad);  // 開始監聽Y軸滑動
-}
-// 偵測Y軸滑動位置接續載入圖片，當Y軸滑動距離 > (整個頁面高度-使用者畫面高度) * 0.75 且尚無執行AJAX讀取資訊，執行fetch方法
-const scrollLoad=()=>{
-    if(window.scrollY > (document.documentElement.scrollHeight-window.innerHeight)*0.75 && loadStatusNone){
-        loadAttractions(pageNum, keywordInput);
-        loadStatusNone=false;           // 更新AJAX讀取狀態為執行中(=false)
+// Models
+let models={
+    // 顯示資料頁數預設為第一頁(index 0)
+    pageNum=0,
+    // 搜尋關鍵字
+    keywordInput=null,
+    // api回復資料
+    data:null,
+    // 向api索取景點資料
+    getData:function(pages, keyword=null){
+        let src="http://35.72.125.150:3000/api/attractions?page="+pages;
+        if(keyword){
+            src += ("&keyword="+keyword);
+        }
+        return fetch(src).then((response)=>{
+            return response.json();
+        }).then((result)=>{
+            this.data=result;
+            this.pageNum=result.nextPage;            
+        });
     }
 };
-// 設定搜尋按鈕被點擊後動作
-let searchBtn=document.getElementById("searchBtn");
-searchBtn.onclick=()=>{
-    keywordInput=document.getElementById("keyword").value;
-    pageNum=0;
-    loadAttractions(pageNum, keywordInput);
-    // 刪除當下呈現的景點資訊或「查無資料」文字資訊
-    let originalAttractions=document.querySelectorAll(".oneContainer");
-    if(originalAttractions.length){
-        for(i=0;i<originalAttractions.length;i++){
-            originalAttractions[i].remove();
-        }
-    }else{
-        let textResult=document.querySelector(".noData");
-        textResult.remove();
-    }
-    window.addEventListener("scroll", scrollLoad);      // 開始監聽Y軸滑動
-}
-// 以fetch方法執行AJAX向api讀取景點資訊
-loadAttractions=(pages, keyword=null)=>{
-    let src="http://35.72.125.150:3000/api/attractions?page="+pages;
-    if(keyword){
-        src += ("&keyword="+keyword);
-    }
-    fetch(src).then((response)=>{
-        return response.json();
-    }).then((result)=>{
-        pageNum=result.nextPage;
-        if(!pageNum){
-            window.removeEventListener("scroll", scrollLoad);   // 無nextpage結束Y軸滑動監聽
-        }
-        if(result.data.length){
-            showAttractions(result.data);
-        }else{
-            let parent=document.querySelector(".mainContainer");
-            let title=document.createElement("div");
-            title.classList.add("noData");
-            title.textContent="查無資料。";
-            parent.appendChild(title);
-        }
-    })
-}
-// 將api回傳景點資訊呈現在頁面，且更新AJAX讀取狀態為尚未執行(=true)
-showAttractions=(data)=>{
-    for(i=0;i<data.length;i++){
+// Views
+let views={
+    // 顯示景點資料
+    renderData:function(data){
         let parent=document.querySelector(".mainContainer");
-        let oneContainer=document.createElement("div");
-        oneContainer.classList.add("oneContainer");
-        const firstUrl=data[i].images[0];
-        let image=document.createElement("img");
-        image.src=firstUrl;
-        let attraction=document.createElement("div");
-        attraction.classList.add("attraction");
+        parent.innerHTML="";
+        for(i=0;i<data.length;i++){
+            let parent=document.querySelector(".mainContainer");
+            let oneContainer=document.createElement("div");
+            oneContainer.classList.add("oneContainer");
+            const firstUrl=data[i].images[0];
+            let image=document.createElement("img");
+            image.src=firstUrl;
+            let attraction=document.createElement("div");
+            attraction.classList.add("attraction");
+            let title=document.createElement("div");
+            title.classList.add("title");
+            title.textContent=data[i].name;
+            let subtitle=document.createElement("div");
+            subtitle.classList.add("subtitle");
+            let subLeft=document.createElement("div");
+            subLeft.classList.add("subLeft");
+            subLeft.textContent=data[i].mrt;
+            let subRight=document.createElement("div");
+            subRight.classList.add("subRight");
+            subRight.textContent=data[i].category;
+            let hyperLink=document.createElement("a");
+            hyperLink.href="/attraction/"+data[i].id;
+            let spanLink=document.createElement("span");
+            spanLink.classList.add("spanLink");
+            hyperLink.appendChild(spanLink);
+            subtitle.appendChild(subLeft);
+            subtitle.appendChild(subRight);
+            attraction.appendChild(title);
+            attraction.appendChild(subtitle);
+            oneContainer.appendChild(image);
+            oneContainer.appendChild(attraction);
+            oneContainer.appendChild(hyperLink);
+            parent.appendChild(oneContainer);
+        }
+        controllers.loadStatusNone=true;
+    },
+    // 顯示無資料
+    renderNoneData:function(){
+        let parent=document.querySelector(".mainContainer");
+        parent.innerHTML="";
         let title=document.createElement("div");
-        title.classList.add("title");
-        title.textContent=data[i].name;
-        let subtitle=document.createElement("div");
-        subtitle.classList.add("subtitle");
-        let subLeft=document.createElement("div");
-        subLeft.classList.add("subLeft");
-        subLeft.textContent=data[i].mrt;
-        let subRight=document.createElement("div");
-        subRight.classList.add("subRight");
-        subRight.textContent=data[i].category;
-        let hyperLink=document.createElement("a");
-        hyperLink.href="/attraction/"+data[i].id;
-        let spanLink=document.createElement("span");
-        spanLink.classList.add("spanLink");
-        hyperLink.appendChild(spanLink);
-        subtitle.appendChild(subLeft);
-        subtitle.appendChild(subRight);
-        attraction.appendChild(title);
-        attraction.appendChild(subtitle);
-        oneContainer.appendChild(image);
-        oneContainer.appendChild(attraction);
-        oneContainer.appendChild(hyperLink);
-        parent.appendChild(oneContainer);
+        title.classList.add("noData");
+        title.textContent="查無資料。";
+        parent.appendChild(title);
     }
-    loadStatusNone=true;
-}
+};
+// Controllers
+let controllers={
+    // AJAX讀取狀態
+    loadStatusNone=false,    
+    // 初始化函式 呈現景點資料並啟動物件監聽
+    init:function(){
+        models.getData(models.pageNum).then(()=>{
+            views.renderData(models.data);
+        }).then(()=>{
+            this.searchButtonListener();
+            this.scrollLoadListener();
+        });
+    },
+    // 監聽使用者點擊搜尋
+    searchButtonListener:function(){
+        let searchBtn=document.getElementById("searchBtn");
+        searchBtn.addEventListener("click", function(){
+            models.keywordInput=document.getElementById("keyword").value;
+            models.pageNum=0;
+            models.getData(models.pageNum, models.keywordInput).then(()=>{
+                if(models.data.length){
+                    views.renderData(models.data);
+                }else{
+                    views.renderNoneData();
+                };
+            });
+        });
+    },
+    // 監聽無限卷軸做動
+    // 偵測Y軸滑動位置接續載入圖片，當Y軸滑動距離 > (整個頁面高度-使用者畫面高度) * 0.75 且尚無執行AJAX讀取資訊，執行fetch方法
+    scrollLoadListener:function(){
+        if(window.scrollY > (document.documentElement.scrollHeight-window.innerHeight)*0.75 && loadStatusNone){
+            models.getData(models.pageNum, models.keywordInput);
+            this.loadStatusNone=false;           // 更新AJAX讀取狀態為執行中(=false)
+        };
+    }
+};
+controllers.init();     //載入頁面初始化並啟動物件監聽
